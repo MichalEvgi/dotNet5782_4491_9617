4780e98ab7d84e369e8d4b9caef7afa184e038a1
MichalSuper <superfin@g.jct.ac.il>
2021-10-10 22:23:43 +0300

    I am here function

---
+++ Targil0/Program9617.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Targil0
+{
+   partial class Program
+    {
+        static partial void Welcome9617()
+        {
+            Console.WriteLine("I am the first one here!");
+        }
+    }
+}
