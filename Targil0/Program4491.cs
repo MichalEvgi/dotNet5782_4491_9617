e221bc5b5a15f7be62e103ad6e5a9c46d5b38e0a
MichalEvgi <mevgi@g.jct.ac.il>
2021-10-10 21:39:00 +0300

    intial code+ Welcome functions

---
+++ Targil0/Program4491.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Targil0
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome4491();
+            Welcome9617();
+            Console.ReadKey();
+        }
+        static partial void Welcome9617();
+        private static void Welcome4491()
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", name);
+        }
+    }
+}
