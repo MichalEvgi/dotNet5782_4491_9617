554fd1e28b0cd84654dc1865ffdab85286a4cb62
MichalEvgi <mevgi@g.jct.ac.il>
2022-01-14 14:57:51 +0200

    hint

--- PL/ParcelListWindow.xaml.cs
+++ PL/ParcelListWindow.xaml.cs
@@ -27,6 +27,17 @@ namespace PL
             bl = bL;
             InitializeComponent();
             ParcelListView.ItemsSource = bl.GetParcelsList().ToList();
+            foreach (ParcelModes pm in (ParcelModes[])Enum.GetValues(typeof(ParcelModes)))
+            {
+                ParcelModecmb.Items.Add(pm);
+            }
+            ParcelModecmb.Items.Add("Clear filter");
+
+            foreach (Priorities pm in (Priorities[])Enum.GetValues(typeof(Priorities)))
+            {
+                Prioritycmb.Items.Add(pm);
+            }
+            Prioritycmb.Items.Add("Clear filter");
 
         }
         private void ParcelListView_MouseDoubleClick(object sender, MouseButtonEventArgs e)
@@ -42,6 +53,85 @@ namespace PL
         private void AddParcel_Click(object sender, RoutedEventArgs e)
         {
             new ParcelWindow(bl, this).Show();
+        }
+
+        private void SenderGrop_Click(object sender, RoutedEventArgs e)
+        {
+            IEnumerable<IGrouping<string, ParcelToList>> parcelGroup = from par in bl.GetParcelsList() group par by par.SenderName;
+            List<ParcelToList> parcelTos = new();
+
+            foreach (var group in parcelGroup)
+            {
+                foreach (var par in group)
+                {
+                    parcelTos.Add(par);
+                }
+            }
+            ParcelListView.ItemsSource = parcelTos;
+
+
+            CollectionView view = (CollectionView)CollectionViewSource.GetDefaultView(ParcelListView.ItemsSource);
+            if (view.GroupDescriptions.Count < 1)
+            {
+                PropertyGroupDescription groupDescription = new PropertyGroupDescription("SenderName");
+                view.GroupDescriptions.Add(groupDescription);
+            }
+        }
+
+        private void TargetGroup_Click(object sender, RoutedEventArgs e)
+        {
+            IEnumerable<IGrouping<string, ParcelToList>> parcelGroup = from par in bl.GetParcelsList() group par by par.TargetName;
+            List<ParcelToList> parcelTos = new();
+
+            foreach (var group in parcelGroup)
+            {
+                foreach (var par in group)
+                {
+                    parcelTos.Add(par);
+                }
+            }
+            ParcelListView.ItemsSource = parcelTos;
+
+
+            CollectionView view = (CollectionView)CollectionViewSource.GetDefaultView(ParcelListView.ItemsSource);
+            if (view.GroupDescriptions.Count < 1)
+            {
+                PropertyGroupDescription groupDescription = new PropertyGroupDescription("TargetName");
+                view.GroupDescriptions.Add(groupDescription);
+            }
+        }
+
+        private void ParcelModecmb_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            SelectorChanges();
+        }
+        public void SelectorChanges()
+        {
+            if ((ParcelModecmb.SelectedItem == null || ParcelModecmb.SelectedItem.ToString() == "Clear filter") && (Prioritycmb.SelectedItem == null || Prioritycmb.SelectedItem.ToString() == "Clear filter"))
+            {
+                //if both fiter aren't set
+                ParcelListView.ItemsSource = bl.GetParcelsList().Select(x => x);
+            }
+            else
+            {
+                if (ParcelModecmb.SelectedItem == null || ParcelModecmb.SelectedItem.ToString() == "Clear filter")
+                    //if there is only priority filter
+                    ParcelListView.ItemsSource = bl.GetParcelsList().Where(p => p.Priority == (Priorities)Prioritycmb.SelectedItem);
+                else
+                {
+                    if (Prioritycmb.SelectedItem == null || Prioritycmb.SelectedItem.ToString() == "Clear filter")
+                        //if there is only mode filter
+                        ParcelListView.ItemsSource = bl.GetParcelsList().Where(p => p.ParcelMode == (ParcelModes)ParcelModecmb.SelectedItem);
+                    else
+                        //if the two filter were selected
+                        ParcelListView.ItemsSource = bl.GetParcelsList().Where(p => (p.Priority == (Priorities)Prioritycmb.SelectedItem) && (p.ParcelMode == (ParcelModes)ParcelModecmb.SelectedItem));
+                }
+            }
+        }
+
+        private void Prioritycmb_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            SelectorChanges();
         }
     }
 }
