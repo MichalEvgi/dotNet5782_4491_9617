dba6d856ae8121b20ab920880afbe733c2b940dd
MichalEvgi <mevgi@g.jct.ac.il>
2022-01-17 05:21:30 +0200

    simulator

--- BL/BL/Simulator.cs
+++ BL/BL/Simulator.cs
@@ -18,19 +18,18 @@ namespace BL
         Drone drone;
         DroneToList dro;
         int check;
-        double intialBattery = 0;
+        Location dest;
+        double progresLon, progresLat, progresBat;
+        double dis, flyTime;
         public Simulator(int droneId, Action reportProgress, Func<bool> stopDelgate, BL bl)
         {
             lock (bl)
             {
                 dro = bl.drones.Where(d => d.Id == droneId).FirstOrDefault();
+                drone = bl.GetDrone(droneId);
             }
             while (!stopDelgate())
-            {
-                lock (bl)
-                {
-                    drone = bl.GetDrone(droneId);
-                }
+            { 
                 switch (drone.Status)
                 {
                     case DroneStatus.Available:
@@ -39,18 +38,18 @@ namespace BL
                             try
                             {
                                 bl.DroneToParcel(droneId);
+                                drone = bl.GetDrone(droneId);
                             }
                             catch (Exception)
                             {
-                                //try
-                                //{
-                                bl.SendToCharge(droneId);
-                                reportProgress();
-                                //}
-                                //catch (BatteryException)
-                                //{
-                                //    Thread.Sleep(DELAY);
-                                //}
+                                try
+                                {
+                                    bl.SendToCharge(droneId);
+                                drone = bl.GetDrone(droneId);
+                                }
+                                catch (BatteryException)
+                                {
+                                }
                             }
                         }
                         break;
@@ -60,68 +59,70 @@ namespace BL
                             bl.ReleaseDrone(droneId);
                             if (drone.Battery < 100)
                                 bl.SendToCharge(droneId);
-                            reportProgress();
+                            drone = bl.GetDrone(droneId);
                         }
                         break;
                     case DroneStatus.Delivery:
-                        double flyTime = drone.TransferedParcel.Distance / SPEED;
-                        Location dest;
-                        double progresLon;
-                        double progresLat;
-                        double progresBat;
-                        
                         if (bl.GetParcel(drone.TransferedParcel.Id).PickedUpTime == null)//haven't picked up yet
                         {
                             dest = drone.TransferedParcel.SourceLocation;
+                            dis = bl.dal.Distance(drone.CurrentLocation.Longitude, drone.CurrentLocation.Lattitude,dest.Longitude, dest.Lattitude);
+                            flyTime = dis / SPEED;
                             progresLon = (dest.Longitude - drone.CurrentLocation.Longitude) / flyTime;
                             progresLat = (dest.Lattitude - drone.CurrentLocation.Lattitude) / flyTime;
                             lock (bl)
                             {
-                                progresBat = bl.dal.ElectricityRequest().First() * drone.TransferedParcel.Distance;
+                                progresBat = bl.dal.ElectricityRequest().First() * dis;
                             }
-                            if (check < drone.TransferedParcel.Distance)
+                            if (check < dis)
                             {
                                 dro.CurrentLocation.Longitude += progresLon;
                                 dro.CurrentLocation.Lattitude += progresLat;
                                 dro.Battery -= progresBat / flyTime;
-                                intialBattery+= progresBat / flyTime;
                                 check += 1;
                                 break;
                             }
-                            dro.Battery += intialBattery;
-                            intialBattery = 0;
                             lock (bl)
                             {
                                 bl.PickParcel(droneId);
                             }
+                            dro.Battery += progresBat;
                             check = 0;
+                            lock (bl)
+                            {
+                                drone = bl.GetDrone(droneId);
+                            }
                             break;
                         }
                         else //already picked up
                         {
                             dest = drone.TransferedParcel.DestinationLocation;
+                            dis = bl.dal.Distance(drone.CurrentLocation.Longitude, drone.CurrentLocation.Lattitude, dest.Longitude, dest.Lattitude);
+                            flyTime = dis / SPEED;
                             progresLon = (dest.Longitude - drone.CurrentLocation.Longitude) / flyTime;
                             progresLat = (dest.Lattitude - drone.CurrentLocation.Lattitude) / flyTime;
                             lock (bl)
                             {
-                                progresBat = bl.dal.ElectricityRequest().ElementAt((int)drone.TransferedParcel.Weight + 1) * drone.TransferedParcel.Distance;
+                                progresBat = bl.dal.ElectricityRequest().ElementAt((int)drone.TransferedParcel.Weight + 1) * dis;
                             }
-                            check += 1;
-                            if (check < drone.TransferedParcel.Distance)
+                            if (check < dis)
                             {
                                 dro.CurrentLocation.Longitude += progresLon;
                                 dro.CurrentLocation.Lattitude += progresLat;
                                 dro.Battery -= progresBat / flyTime;
-                                intialBattery+= progresBat / flyTime;
+                                check += 1;
                                 break;
                             }
-                            dro.Battery += intialBattery;
-                            intialBattery = 0;
                             lock (bl)
                             {
                                 bl.DeliverParcel(droneId);
                             }
+                            dro.Battery += progresBat;
                             check = 0;
+                            lock (bl)
+                            {
+                                drone = bl.GetDrone(droneId);
+                            }
                             break;
                         }
                 }
